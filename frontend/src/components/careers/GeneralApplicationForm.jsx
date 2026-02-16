45b0a6eb7d2fdd2ceb2c7bd27e5d6fe5ef3c56d3
Computer-TR <computertrinc@gmail.com>
2026-02-16 13:46:33 -0600

    Comment out axios import in GeneralApplicationForm

--- frontend/src/components/careers/GeneralApplicationForm.jsx
+++ frontend/src/components/careers/GeneralApplicationForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { applicationService } from "../../services/applicationService";
 
 const GeneralApplicationForm = ({ position, onSuccess, onCancel }) => {
