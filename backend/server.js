eae27d8e0985dbaabf1a765f0ad41236372c8343
Computer-TR <computertrinc@gmail.com>
2026-02-23 08:37:40 -0600

    Add allowed origin for thepetercompanies.com

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(cors({
       'http://localhost:3000',
       'http://localhost:5173',
       'https://peter-trucking-frontend.vercel.app',
+      'https://thepetercompanies.com'',
     ];
 
     const isVercelPreview = /^https:\/\/peter-trucking-frontend.*\.vercel\.app$/.test(origin);
